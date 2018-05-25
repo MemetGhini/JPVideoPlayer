cc887d7b46b08551a59434ce3731264acaf93f08
MemetGhini <memetghini@qq.com>
2018-05-25 15:52:33 +0800

    播放状态添加了从头开始播放

--- JPVideoPlayer/JPVideoPlayer.m
+++ JPVideoPlayer/JPVideoPlayer.m
@@ -463,7 +463,7 @@ didReceiveLoadingRequestTask:(JPResourceLoadingRequestWebTask *)requestTask {
         [strong_Item.player play];
 
         if (self.delegate && [self.delegate respondsToSelector:@selector(videoPlayer:playerStatusDidChange:)]) {
-            [self.delegate videoPlayer:self playerStatusDidChange:JPVideoPlayerStatusPlaying];
+            [self.delegate videoPlayer:self playerStatusDidChange:JPVideoPlayerStatusBeginning];
         }
     }];
 }
@@ -489,7 +489,7 @@ didReceiveLoadingRequestTask:(JPResourceLoadingRequestWebTask *)requestTask {
                 [self.playerModel.player play];
                 [self displayVideoPicturesOnShowLayer];
                 if (self.delegate && [self.delegate respondsToSelector:@selector(videoPlayer:playerStatusDidChange:)]) {
-                    [self.delegate videoPlayer:self playerStatusDidChange:self.playerStatus];
+                    [self.delegate videoPlayer:self playerStatusDidChange:JPVideoPlayerStatusBeginning];
                 }
             }
                 break;
